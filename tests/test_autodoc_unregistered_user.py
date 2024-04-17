c81ff2c22db61f8e9122d6ac1f7362dfb29f0259
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-04-17 20:07:43 +0300

    minor changes

--- tests/test_autodoc_unregistered_user.py
+++ tests/test_autodoc_unregistered_user.py
@@ -6,16 +6,16 @@ from allure_commons.types import Severity
 from autodoc_project.pages.main_page import main_page
 
 
-@allure.tag('Autodoc')
+@allure.tag("Autodoc")
 @allure.severity(Severity.NORMAL)
-@allure.label('MDN78', 'User')
-@allure.feature('Authorization')
-@allure.story('Auth unregistered user')
-@allure.link('https://www.autodoc.ru/', name='Autodoc.ru')
+@allure.label("MDN78", "User")
+@allure.feature("Authorization")
+@allure.story("Auth unregistered user")
+@allure.link("https://www.autodoc.ru/", name="Autodoc.ru")
 def test_authorization_unregistered_user():
     main_page.open()
     unregistered_user = User(
-        username=os.getenv('UNREGISTERED_USER_LOGIN'),
-        password=os.getenv('UNREGISTERED_USER_PASSWORD')
+        username=os.getenv("UNREGISTERED_USER_LOGIN"),
+        password=os.getenv("UNREGISTERED_USER_PASSWORD"),
     )
     main_page.authorization_unregistered_user(unregistered_user)
