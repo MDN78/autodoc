6beee8c208ee2cc368cb47498f4c8009d79513be
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-04-22 21:48:25 +0300

    add mobile tests

--- tests/mobile/conftest.py
+++ tests/mobile/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 from utils import resource
-from utils import attach
+from utils import attach_mobile
 from selene import browser
 from appium import webdriver
 from dotenv import load_dotenv
@@ -31,22 +31,39 @@ def mobile_management(context):
             options.set_capability('remote_url', os.getenv('URL'))
             options.set_capability('platformName', os.getenv('PLATFORM_NAME'))
             options.set_capability('app', resource.relative_from_root(os.getenv('APP')))
-        # from project import config_app
-        # options = config_app.to_driver_options(context=context)
-        browser.config.driver = webdriver.Remote(options.get_capability('remote_url'), options=options)
-        # browser.config.timeout = config_app.TIMEOUT
 
+        # browser.config.driver = webdriver.Remote(options.get_capability('remote_url'), options=options)
+        if context == 'bstack':
+            options.set_capability('remote_url', os.getenv('URL'))
+            options.set_capability('deviceName', os.getenv('DEVICE_NAME'))
+            options.set_capability('platformName', os.getenv('PLATFORM_NAME'))
+            options.set_capability('platformVersion', os.getenv('PLATFORM_VERSION'))
+            options.set_capability('app', os.getenv('APP_M'))
+            options.set_capability(
+                'bstack:options',
+                {
+                    "projectName": "First Python project",
+                    "buildName": "browserstack-build-1",
+                    "sessionName": "BStack first_test",
+                    "userName": os.getenv('USER_NAME'),
+                    "accessKey": os.getenv('ACCESS_KEY'),
+                },
+            )
+    browser.config.driver = webdriver.Remote(options.get_capability('remote_url'), options=options)
     yield
 
-    # with step('Add screenshot'):
-    #     attach.add_screenshot(browser)
-    #
-    # with step('Add html'):
-    #     attach.add_html(browser)
+    with step('Add screenshot'):
+        attach_mobile.add_screenshot(browser)
+
+    with step('Add html'):
+        attach_mobile.add_html(browser)
 
     with step('Close driver'):
         browser.quit()
 
-    # if context == 'bstack':
-    #     with step('Add video'):
-    #         attach.add_video(browser)
+    if context == 'bstack':
+        with step('Add video'):
+            attach_mobile.add_video(browser)
+
+# запуск тестов pytest tests\mobile --context=local_emulator
+# pytest tests\mobile --context=bstack
