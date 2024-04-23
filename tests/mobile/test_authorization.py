1e8bc1556ee892867fcd0ad7490432d81bfcc36a
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-04-23 19:13:16 +0300

    update mobile tests

--- tests/mobile/test_authorization.py
+++ tests/mobile/test_authorization.py
@@ -1,4 +1,10 @@
+import os
+from autodoc_project.data.users import User
 from autodoc_project.pages.mobile_pages.main_page_mobile import android_main_page
 
 def test_authorization():
-    android_main_page.authorization('SPA-49335', 'C602E21C')
+    registered_user = User(
+        username=os.getenv('USER_LOGIN'),
+        password=os.getenv('USER_PASSWORD')
+    )
+    android_main_page.authorization(registered_user.username, registered_user.password)
