18b2b741fc244574031089035e25d445f3027823
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-04-29 10:23:28 +0300

    update README.md

--- tests/api/test_add_item_to_cart.py
+++ tests/api/test_add_item_to_cart.py
@@ -2,11 +2,11 @@ import allure
 from allure_commons.types import Severity
 from autodoc_project.api.board_api import board_api
 
+@allure.feature('API')
 @allure.tag('API')
 @allure.story('Add item to cart via API')
 @allure.title('Add item to cart')
 @allure.severity(Severity.NORMAL)
-@allure.feature('Add to cart')
 @allure.link('https://www.autodoc.ru/', name='Autodoc.ru')
 def test_add_item_to_cart():
     token = board_api.get_auth_token()
--- utils/validator_json.py
+++ utils/validator_json.py
@@ -1,10 +1,17 @@
 import json
+import allure
 import requests
 from utils import resource
+from utils.logger import step
 from jsonschema import validate
 
 
+@step
+@allure.step('API: validate JSON scheme')
 def validator_all_json_scheme(name, url):
-    response = requests.get(url=url)
-    schema = json.load(open(resource.path_json_scheme(file_name=name)))
-    validate(response.json(), schema)
+    with allure.step('Get request'):
+        response = requests.get(url=url)
+    with allure.step('Get JSON scheme'):
+        schema = json.load(open(resource.path_json_scheme(file_name=name)))
+    with allure.step('Validate scheme'):
+        validate(response.json(), schema)
